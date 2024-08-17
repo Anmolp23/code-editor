dda88219d80330a48bbe57625625bb819e291bf4
Anmol Parihar <anmolparihar2494@gmail.com>
2024-08-17 16:56:50 +0530

    My first git commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import CodeEditor from "./components/CodeEditor"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return(
+<div className='bg-black w-full min-h-screen text-gray-200 px-6 py-4'>
+  <CodeEditor />  
+</div>
+);
 }
 
 export default App;
---
+++ src/components/CodeEditor.js
@@ -0,0 +1,44 @@
+import { Editor } from "@monaco-editor/react";
+import React, { useRef, useState } from "react";
+import LanguageSelector from "./LanguageSelector";
+import { CODE_SNIPPETS } from "../constants";
+import { Box, HStack } from "@chakra-ui/react";
+import Output from "./Output";
+
+const CodeEditor = () => {
+    const [value, setValue] = useState('')
+    const editorRef = useRef();
+    const [language, setLanguage] = useState('javascript')
+     
+    const onMount = (editor) => {
+        editorRef.current = editor;
+        editor.focus();
+    }
+
+    const onSelect = (language) => {
+        setLanguage(language)
+        setValue(CODE_SNIPPETS[language]);
+    }
+
+    return (
+        <div className="flex flex-col md:flex-row gap-2">
+            <div className="w-full md:w-2/3 h-3/4 md:h-full">
+                <LanguageSelector language={language} onSelect={onSelect}/>
+                <Editor
+                    className="h-[50vh] md:h-[80vh]"
+                    theme="vs-dark"
+                    language={language}
+                    defaultValue={`${CODE_SNIPPETS[language]}`}
+                    onMount={onMount}
+                    value={value}
+                    onChange={(val) => setValue(val)}
+                />
+            </div>
+            <div className="w-full md:w-1/3 h-1/4 md:h-full">
+            <Output editorRef={editorRef} language={language}/>
+            </div>  
+        </div>
+    );
+};
+
+export default CodeEditor;
